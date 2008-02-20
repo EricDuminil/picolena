ff065db78e635231f324c18dbe359cf7f89442d3
eric.duminil@gmail.com <eric.duminil@gmail.com@354c73f7-f838-4446-8ec5-bf0d774acd2b>
2008-02-20 15:33:51 +0000

    Basic implementation of filters' DSL, with renamed module and classes
    git-svn-id: http://svn.devjavu.com/picolena/branches/with_filters_DSL@24 354c73f7-f838-4446-8ec5-bf0d774acd2b

--- lib/filters/opendocument.text.rb
+++ lib/filters/opendocument.text.rb
@@ -3,15 +3,9 @@
 #   Version tested: 0.3
 #   Home page: http://www.freewisdom.org/projects/python-markdown/odt2txt.php
 
-Filter.which {
-  convert [:doc, :dot]
+PlainText.filter {
+  convert :odf
   as 'application/vnd.oasis.opendocument.text'
   aka "Open Document Format for text"
   with "odt2txt SOURCE > DESTINATION 2>/dev/null" => :on_linux, "some other command" => :on_windows
 }
-
-#  def odt_to_text(src, dst)
-#    %x{odt2txt \"#{src}\" > \"#{dst}\" 2>/dev/null}
-#    raise 'missing odt2txt.py(1) command' if $?.exitstatus == 127
-#    raise "failed to convert Open Document text file: #{src}" unless $?.exitstatus == 0
-#  end
