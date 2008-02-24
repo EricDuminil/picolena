a4a12d957b32d951592aed35f245dcf4d03091aa
eric.duminil@gmail.com <eric.duminil@gmail.com@354c73f7-f838-4446-8ec5-bf0d774acd2b>
2008-02-24 11:54:02 +0000

    "all" keyword can be used in white_list_ip.yml in order to allow every request
    git-svn-id: http://svn.devjavu.com/picolena/trunk@42 354c73f7-f838-4446-8ec5-bf0d774acd2b

--- config/initializers/003_white_list_IPs.rb
+++ config/initializers/003_white_list_IPs.rb
@@ -3,6 +3,6 @@ white_list_ip_config_file=['config/white_list_ip.yml', 'config/white_list_ip.yml
 WhiteListIPs=Regexp.new(
     "^("<<
       YAML.load_file(white_list_ip_config_file)["Allow"].collect{|ip|
-        Regexp.escape(ip)
+        ip.downcase.include?("all") ? /.*/ : Regexp.escape(ip)
       }.join("|")<<")"
   ) rescue /^(127\.0\.0\.1|0\.0\.0\.0)/
