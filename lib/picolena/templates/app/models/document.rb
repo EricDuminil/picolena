b3259fa418b82590b1fdcb395a89a37820bb162a
eric.duminil@gmail.com <eric.duminil@gmail.com@354c73f7-f838-4446-8ec5-bf0d774acd2b>
2008-04-10 09:40:34 +0000

    Some more documentation about Document class.
    absolute_dirname wasn't needed.
    
    git-svn-id: http://svn.devjavu.com/picolena/trunk@132 354c73f7-f838-4446-8ec5-bf0d774acd2b

--- lib/picolena/templates/app/models/document.rb
+++ lib/picolena/templates/app/models/document.rb
@@ -1,41 +1,45 @@
+# Document class retrieves information from filesystem and the index for any given document.
 class Document
   attr_reader :complete_path
   attr_accessor :user, :score, :matching_content, :index_id
   
   def initialize(path)
+    #To ensure @complete_path is an absolute direction.
     @complete_path=File.expand_path(path)
     validate_existence_of_file
     validate_in_indexed_directory
   end
   
-  def to_param
-    id
-  end
-  
-  def to_s
-    filename
-  end
+  alias_method :to_param, :id
   
   #Delegating properties to File::method_name(complete_path)
   [:dirname, :basename, :extname, :size?, :file?, :read, :ext_as_sym].each{|method_name|
     define_method(method_name){File.send(method_name,complete_path)}
   }
   alias_method :size, :size?
-  alias_method :content, :read
   alias_method :filename, :basename
+  alias_method :to_s, :basename
   
+  # Returns filename without extension
+  #   "buildings.odt" => "buildings"
   def basename
     filename.chomp(extname)
   end
-    
-  def absolute_dirname
-    Pathname.new(dirname).realpath.to_s
-  end
   
+  # End users should not always know where documents are stored internally.
+  # An alias path can be specified in config/indexed_directories.yml
+  # 
+  # For example, with:
+  #   "/media/wiki_dump/" : "http://www.mycompany.com/wiki/"
+  # 
+  # The documents
+  #   "/media/wiki_dump/organigram.odp"
+  # will be displayed as being:
+  #   "http://www.mycompany.com/wiki/organigram.odp"
   def alias_path
     original_dir=indexed_directory
     alias_dir=IndexedDirectories[original_dir]
-    absolute_dirname.sub(original_dir,alias_dir)
+    dirname.sub(original_dir,alias_dir)
   end
   
   def probably_unique_id
@@ -46,16 +50,20 @@ class Document
     PlainText.supported_extensions.include?(self.ext_as_sym)
   end
   
+  # Retrieves content as it is *now*.
   def content
     PlainText.extract_content_from(complete_path)
   end
   
-  # retrieve content as it was at the time it was indexed.
+  # Cache à la Google.
+  # Returns content as it was at the time it was indexed.
   def cached
     get_index_id! unless index_id
     Finder.index[index_id][:content]
   end
   
+  # Returns the last modification date before the document got indexed.
+  # Useful to know how old a document is, and to which version the cache corresponds.
   def date
     get_index_id! unless index_id
     Finder.index[index_id][:date].sub(/(\d{4})(\d{2})(\d{2})/,'\1-\2-\3')
@@ -77,7 +85,7 @@ class Document
   
   def indexed_directory
     IndexedDirectories.keys.find{|indexed_dir|
-      absolute_dirname.starts_with?(indexed_dir)
+      dirname.starts_with?(indexed_dir)
     }    
   end
   
