da87ec6e70a3d4a8d898753c446927c112847e18
eric.duminil@gmail.com <eric.duminil@gmail.com@354c73f7-f838-4446-8ec5-bf0d774acd2b>
2008-04-10 11:39:14 +0000

    Easier to specify custom page title or help file.
    git-svn-id: http://svn.devjavu.com/picolena/trunk@135 354c73f7-f838-4446-8ec5-bf0d774acd2b

--- lib/picolena/templates/config/initializers/005_load_custom_localization.rb
+++ lib/picolena/templates/config/initializers/005_load_custom_localization.rb
@@ -1,5 +1,5 @@
 custom_localization_yml=File.join(RAILS_ROOT,'lang/ui/custom_localization.yml')
 
-YAML.load_file(custom_localization_yml).each{|key_name, translation|
-  Globalite.localizations[key_name.to_sym]=translation
+YAML.load_file(custom_localization_yml).each{|key_name, custom_translation|
+  Globalite.localizations[key_name.to_sym]=custom_translation unless custom_translation.blank?
 }
