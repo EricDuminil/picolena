d6c4c9d5fe33885067b88bff3168449c6ab96c68
eric.duminil@gmail.com <eric.duminil@gmail.com@354c73f7-f838-4446-8ec5-bf0d774acd2b>
2008-04-12 12:24:38 +0000

    Nothing left from FerretHelper!
    (Big thanks to Stuart, though)
    
    git-svn-id: http://svn.devjavu.com/picolena/branches/rewrote_indexer@150 354c73f7-f838-4446-8ec5-bf0d774acd2b

--- lib/picolena/templates/app/models/indexer.rb
+++ lib/picolena/templates/app/models/indexer.rb
@@ -1,72 +1,73 @@
 class Indexer
   Exclude = /(Thumbs\.db)/
   
-  def self.index_every_directory
-    log :debug => "Indexing every directory"
-    begin
+  class << self
+    def fields_for(filename)
+      {
+        :complete_path=> complete_path=File.expand_path(filename),
+        :probably_unique_id => complete_path.base26_hash,
+        :file => File.basename(filename),
+        :basename => File.basename(filename, File.extname(filename)).gsub(/_/,' '),
+        :filetype => File.extname(filename),
+        :date => File.mtime(filename).strftime("%Y%m%d%H%M")
+      }      
+    end
+    
+    def index_every_directory
+      log :debug => "Indexing every directory"
+      
       IndexedDirectories.each{|dir, alias_dir|
         index_directory(dir)
       }
       writer.optimize
-    ensure
       writer.close
     end
-  end
-  
-  def self.index_directory(dir)
-    log :debug => "Indexing #{dir}"
-    Dir.glob(File.join(dir,"**/*")){|filename|
-      if File.file?(filename) && filename !~ Exclude then
-        mime_type = File.mime(filename)
-        begin
-          index_file(filename, mime_type)
-        rescue => e
-          log :debug => "indexing without content: #{e.message}"
-          index_file(filename)
-        end
-      end
-    }
-  end
-  
-  def self.index_file(filename,mime_type = nil)
-    log :debug => "Indexing #{filename}"
     
-    complete_path=File.expand_path(filename)
     
-    fields = {
-      :complete_path=> complete_path,
-      :probably_unique_id => complete_path.base26_hash,
-      :file => File.basename(filename),
-      :basename => File.basename(filename, File.extname(filename)).gsub(/_/,' '),
-      :filetype => File.extname(filename),
-      :date => File.mtime(filename).strftime("%Y%m%d%H%M")
-    }
-    
-    if mime_type then
-      text = PlainText.extract_content_from(filename)
-      raise "empty document #{filename}" if text.strip.empty?
-      fields[:content] = text
+    def index_directory(dir)
+      log :debug => "Indexing #{dir}"
+      
+      Dir.glob(File.join(dir,"**/*")){|filename|
+        index_file(filename, File.mime(filename)) if File.file?(filename) && filename !~ Exclude
+      }
     end
     
-    writer << fields
-  end
-  
-  def self.writer
-    @@writer ||= IndexWriter.new
-  end
-  
-  def self.reset!
-    log :debug => "Resetting Index"
-    @@writer=nil
-    IndexWriter.remove
-  end
-  
-  private
-  
-  def self.log(hash)
-    hash.each{|level,message|
-      puts "#{level} -> #{message}"
-      #IndexerLogger.send(level,message)
-    }
+    
+    def index_file(filename,mime_type = nil)
+      log :debug => "Indexing #{filename}"
+      
+      fields = fields_for(filename)
+      
+      if mime_type then
+        begin 
+          text = PlainText.extract_content_from(filename)
+          raise "empty document #{filename}" if text.strip.empty?
+          fields[:content] = text
+        rescue => e
+          log :debug => "indexing without content: #{e.message}"
+        end
+      end
+      
+      writer << fields
+    end
+    
+    def writer
+      @@writer ||= IndexWriter.new
+    end
+    
+    def reset!
+      log :debug => "Resetting Index"
+      @@writer=nil
+      IndexWriter.remove
+    end
+    
+    private
+    
+    def log(hash)
+      hash.each{|level,message|
+        #puts "#{level} -> #{message}"
+        IndexerLogger.send(level,message)
+      }
+    end  
   end
 end
