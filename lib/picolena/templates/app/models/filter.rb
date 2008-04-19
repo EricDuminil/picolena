8577a28b791d13574520f6879fbf02259ee84bd9
eric.duminil@gmail.com <eric.duminil@gmail.com@354c73f7-f838-4446-8ec5-bf0d774acd2b>
2008-04-19 19:22:52 +0000

    Some doc for models/filter.rb
    git-svn-id: http://svn.devjavu.com/picolena/branches/oo_indexer@174 354c73f7-f838-4446-8ec5-bf0d774acd2b

--- lib/picolena/templates/app/models/filter.rb
+++ lib/picolena/templates/app/models/filter.rb
@@ -3,48 +3,53 @@ require 'filter_DSL'
 class Filter
   include FilterDSL
   @@filters=[]
-  
-  #returns every defined filter
-  def self.all
+  class<<self 
+  # Returns every defined filter
+  def all
     @@filters
   end
-
-  def self.add(filter)
+  
+  # Add a filter to the filters list
+  def add(filter)
     @@filters<<filter
   end
 
-  def self.each(&block)
+  # Calls block for each filter
+  def each(&block)
     all.each(&block)
   end
   
-  #returns every required dependency for every defined filter
-  def self.dependencies
+  # Returns every required dependency for every defined filter
+  def dependencies
     @@dependencies||=all.collect{|filter| filter.dependencies}.flatten.compact.uniq.sort
   end
   
-  #returns every supported file extensions
-  def self.supported_extensions
+  # Returns every supported file extensions
+  def supported_extensions
     @@supported_exts||=all.collect{|filter| filter.exts}.flatten.compact.uniq
   end
 
-  #finds which filter should be used for a given file, according to its extension
-  def self.find_filter_for(filename)
+  # Finds which filter should be used for a given file, according to its extension
+  # Raises if the file is unsupported. 
+  def find_by_filename(filename)
     ext=File.ext_as_sym(filename)
     filter=all.find{|filter| filter.exts.include?(ext)} || raise(ArgumentError, "no convertor for #{filename}")
     filter.source=filename
     filter
   end
   
-  #launches filter on given file and outputs plain text result
-  def self.extract_content_from(source)
-    find_filter_for(source).extract_content
+  # Launches filter on given file and outputs plain text result
+  def extract_content_from(source)
+    find_by_filename(source).extract_content
   end
+  end
+
   
   
     attr_accessor :source
     
-    #parses command in order to know which programs are needed.
-    #rspec will then check that every dependecy is installed on the system
+    # Parses command in order to know which programs are needed.
+    # rspec will then check that every dependecy is installed on the system
     def dependencies
       if command.is_a?(String) then
         command.split(/\|\s*/).collect{|command_part| command_part.split(/ /).first}
@@ -53,20 +58,21 @@ class Filter
       end
     end
     
-    #Conversion part
+    ## Conversion part
     
-    #destination method can be used by some conversion command that cannot output to stdout (example?)
-    #a file containing plain text result will first be written by command, and then be read by extract_content.
+    # destination method can be used by some conversion command that cannot output to stdout (example?)
+    # a file containing plain text result will first be written by command, and then be read by extract_content.
     def destination
       require 'tmpdir'
       @@temp_file_as_destination ||= File.join(Dir::tmpdir,"ferret_#{Time.now.to_i}")
     end
     
-    #Replaces generic command with specific source and destination (if specified) files
+    # Replaces generic command with specific source and destination (if specified) files
     def specific_command
       command.sub('SOURCE','"'<<source<<'"').sub('DESTINATION','"'<<destination<<'"')
     end
-    
+   
+    # Returns plain text content of source file
     def extract_content
       if command.is_a?(String) then
         if command.include?('DESTINATION') then
