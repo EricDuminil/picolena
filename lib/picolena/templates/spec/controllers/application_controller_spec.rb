c5f74428d96bdebe72d703f2fae9a5da089a026e
eric.duminil@gmail.com <eric.duminil@gmail.com@354c73f7-f838-4446-8ec5-bf0d774acd2b>
2008-04-22 11:40:49 +0000

    Access denied specs. Preparing views specs.
    git-svn-id: http://svn.devjavu.com/picolena/trunk@215 354c73f7-f838-4446-8ec5-bf0d774acd2b

--- lib/picolena/templates/spec/controllers/application_controller_spec.rb
+++ lib/picolena/templates/spec/controllers/application_controller_spec.rb
@@ -1,12 +1,17 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe ApplicationController do
-  it "should give 403 when denying access" do
+  it "should give 403 status when denying access" do
     get 'access_denied'
     response.headers["Status"].should == "403 Forbidden"
   end
   
-  it "should flash with a wrong request" do
+  it "should render 'Access denied' text when denying access" do
+    get 'access_denied'
+    response.body.should == 'Access denied'
+  end
+  
+  it "should flash a warning if given a wrong request" do
     get 'unknown_request'
     response.should be_redirect
     response.should redirect_to(documents_url)
