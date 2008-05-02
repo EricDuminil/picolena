00021a13b5a8bdb8783db9e9a021ed1b3cb6b290
eric.duminil@gmail.com <eric.duminil@gmail.com@354c73f7-f838-4446-8ec5-bf0d774acd2b>
2008-05-02 13:29:57 +0000

    removing deprecated spec
    git-svn-id: http://svn.devjavu.com/picolena/trunk@308 354c73f7-f838-4446-8ec5-bf0d774acd2b

--- lib/picolena/templates/spec/helpers/documents_helper_spec.rb
+++ lib/picolena/templates/spec/helpers/documents_helper_spec.rb
@@ -1,8 +1,6 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe DocumentsHelper do
-  it "shouldn't raise if matching not in content field"
-
   PlainTextExtractor.supported_extensions.each{|ext|
     it "should have an icon for .#{ext} filetype" do
       icon_for(ext).should_not be_nil
