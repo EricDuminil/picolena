6fd57aa859f388e104964ff1bca6095804b25f5b
Eric Duminil <eric.duminil@gmail.com>
2008-05-12 19:10:41 +0200

    Initializing Indexer.index.writer sometimes removes Picolena::MetaIndexPath. Why???

--- lib/picolena/templates/spec/models/indexer_spec.rb
+++ lib/picolena/templates/spec/models/indexer_spec.rb
@@ -2,7 +2,9 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Indexer do
   it "should have at least 32MB memory allocated" do
+    # FIXME : Sometimes erases Picolena::MetaIndexPath. WTF?
     Indexer.index.writer.max_buffer_memory.should > 2**25-1
+    FileUtils.mkpath(Picolena::MetaIndexPath)
   end
 
   it "should know the time it was updated" do
