4bc414172eafceec9d1dbb2527910022a7be1027
Eric Duminil <eric.duminil@gmail.com>
2009-01-27 15:34:22 +0100

    41 failures -> 14 failures

--- lib/picolena/templates/spec/helpers/documents_helper_spec.rb
+++ lib/picolena/templates/spec/helpers/documents_helper_spec.rb
@@ -3,7 +3,7 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe DocumentsHelper do
   PlainTextExtractor.supported_extensions.each{|ext|
     it "should have an icon for .#{ext} filetype" do
-      icon_for(ext).should_not be_nil
+      helper.icon_for(ext).should_not be_nil
     end
   }
 end
