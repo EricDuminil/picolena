d382a0da3d90f3277fa4699cdf4ef4dd5b6dbbc0
Eric Duminil <eric.duminil@gmail.com>
2009-02-04 18:11:17 +0100

    Purely aesthetical commit

--- lib/picolena/templates/lib/plain_text_extractors/adobe.photoshop.rb
+++ lib/picolena/templates/lib/plain_text_extractors/adobe.photoshop.rb
@@ -2,9 +2,11 @@ PlainTextExtractor.new {
   every :psd
   as "image/adobe.photoshop"
   aka "Adobe Photoshop Format"
-  extract_content_with 'exiftool SOURCE'
-  #NOTE: PSD gets its own Extractor since convert method is different from one-layer pictures
-  extract_thumbnail_with 'convert SOURCE -flatten -thumbnail 80x80 -quality 50 THUMBNAIL'
 
+  #NOTE: PSD gets its own Extractor since convert method is different from one-layer pictures
+  #      and needs -flatten option
+  extract_thumbnail_with           'convert SOURCE -flatten -thumbnail 80x80 -quality 50 THUMBNAIL'
+
+  extract_content_with             'exiftool SOURCE'
   which_should_for_example_extract '"Adobe Photoshop CS2 Windows" 584x150', :from => 'picolena.psd'
 }
--- lib/picolena/templates/lib/plain_text_extractors/avi.rb
+++ lib/picolena/templates/lib/plain_text_extractors/avi.rb
@@ -4,8 +4,9 @@ PlainTextExtractor.new {
   every :avi
   as "video"
   aka "avi video"
-  extract_content_with 'exiftool SOURCE'
-  extract_thumbnail_with "ffmpegthumbnailer -i SOURCE -o THUMBNAIL"
 
+  extract_thumbnail_with           'ffmpegthumbnailer -i SOURCE -o THUMBNAIL'
+
+  extract_content_with             'exiftool SOURCE'
   which_should_for_example_extract 'Image Size 320x200 Duration 1.96s', :from => 'badminton.avi'
 }
