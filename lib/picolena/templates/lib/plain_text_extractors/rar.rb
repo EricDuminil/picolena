66d947630fe0ccc90012306757192be0cf280ab1
Eric Duminil <eric.duminil@gmail.com>
2009-02-09 13:40:23 +0100

    Unrar can be either free or non-free

--- lib/picolena/templates/lib/plain_text_extractors/rar.rb
+++ lib/picolena/templates/lib/plain_text_extractors/rar.rb
@@ -3,7 +3,14 @@ PlainTextExtractor.new {
   as "archive/rar"
   aka "RAR Archive"
 
-  extract_content_from_archive_with "unrar x SOURCE TEMPDIR"
+  # If a non-free version of unrar is available, uses it
+  # because unrar-nonfree supports more archives than  unrar-free
+  if "unrar".installed? then
+   extract_content_from_archive_with "unrar x SOURCE TEMPDIR"
+  else
+   # falls back to unrar-free otherwise
+   extract_content_from_archive_with "unrar-free --extract SOURCE TEMPDIR"
+  end
 
   which_should_for_example_extract 'IAE2ORREucIRPx+XgpYcYoO8Twz1TN5/LezRbdwWonlAqpDanBTR+McCehXpk7Pz',
                                                                               :from => 'dumb_file.rar'
