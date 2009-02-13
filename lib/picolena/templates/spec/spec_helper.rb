b159f93871681aaebed063b36e8960724f598bb9
Eric Duminil <eric.duminil@gmail.com>
2009-02-14 00:14:10 +0100

    No need for rspec monkey patching anymore

--- lib/picolena/templates/spec/spec_helper.rb
+++ lib/picolena/templates/spec/spec_helper.rb
@@ -17,13 +17,3 @@ class Document
     Finder.new("ext:#{ext}").matching_documents.first
   end
 end
-
-
-## Rspec-rails assume that everybody uses ActiveRecord, but that's not Picolena's case.
-module Spec
-  module Matchers
-    class Change
-      alias_method :evaluate_value_proc, :evaluate_value_proc_without_ensured_evaluation_of_proxy
-    end
-  end
-end
