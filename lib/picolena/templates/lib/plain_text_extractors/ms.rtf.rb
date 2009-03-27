bd37d7de88305f26754ac904cc31516c310d6fbc
Eric Duminil <eric.duminil@gmail.com>
2009-03-27 14:45:36 +0100

    Extracting thumbnails from rtf files

--- lib/picolena/templates/lib/plain_text_extractors/ms.rtf.rb
+++ lib/picolena/templates/lib/plain_text_extractors/ms.rtf.rb
@@ -10,5 +10,15 @@ PlainTextExtractor.new {
   aka "Microsoft Rich Text Format"
   extract_content_with "unrtf  SOURCE -t text" => :on_linux_and_mac_os,
                        "some other command" => :on_windows
+
+  extract_thumbnail_with {|source, destination|
+    wmf_pics_inside_rtf=Dir.glob("#{RAILS_ROOT}/pict*.wmf")
+    unless wmf_pics_inside_rtf.empty?
+      silently_execute("convert -quality 50 -thumbnail 80x80 #{wmf_pics_inside_rtf.first} #{destination}")
+      wmf_pics_inside_rtf.each{|f| FileUtils.rm f}
+    end
+  }
+  which_requires "wmf2eps"
+
   which_should_for_example_extract 'Resampling when limiting', :from => 'ReadMe.rtf'
 }
